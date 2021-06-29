23c7e75704054789d15f79b31b0c521b8556e24a
devsmartify <84731734+devsmartify@users.noreply.github.com>
2021-06-29 03:17:53 +0200

    Update main.js

--- packages/pancake-uikit/.storybook/main.js
+++ packages/pancake-uikit/.storybook/main.js
@@ -1,6 +1,8 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
+     '@storybook/addon-docs'
+    '@storybook/addon-controls'
     {
       name: "@storybook/addon-essentials",
       options: {
